11b6da00d4ddd8f4d1fbcb007dbbef9158a9862b
Aiyaz Mostofa <azmostofa06@gmail.com>
2022-11-27 00:58:03 -0600

    reorganize and change themes

--- init.el
+++ init.el
@@ -53,7 +53,7 @@
   :config (evil-collection-init))
 
 ;; Setup theme
-(use-package doom-themes :config (load-theme 'doom-gruvbox t))
+(use-package base16-theme :config (load-theme 'base16-ayu-dark t))
 
 ;; Setup ivy
 (use-package ivy :config (ivy-mode 1))
@@ -77,18 +77,6 @@
 ;; Setup projectile
 (use-package projectile :config (projectile-mode 1))
 
-;; Setup lsp-mode
-(use-package lsp-mode
-  :hook (
-	 (java-mode . lsp-deferred)
-	 (lsp-mode . lsp-enable-which-key-integration))
-  :commands (lsp lsp-deferred)
-  :config
-  (setq lsp-enable-snippet nil)
-  (setq lsp-headerline-breadcrumb-enable nil))
-
-(use-package lsp-java :ensure t)
-
 ;; Setup yasnippet
 (use-package yasnippet
   :config (yas-global-mode 1))
@@ -106,55 +94,7 @@
   (sp-local-pair 'java-mode "{" nil :post-handlers '(("|| " "SPC") ("||\n[i]" "RET")))
   (electric-indent-mode 1))
 
-;; Some java specific commands
-(defun java-compile-file ()
-  "This function is meant for compiling the current Java file."
-  (interactive)
-  (lsp-format-buffer)
-  (lsp-organize-imports)
-  (save-buffer)
-  (projectile-run-async-shell-command-in-root (concat
-	 (concat "javac src/" (file-relative-name buffer-file-name projectile-project-root))
-	 " -d out/")))
-
-(defun java-run-file ()
-  "This function is meant for running the last compiled version of the current Java file."
-  (interactive)
-  (projectile-run-async-shell-command-in-root (concat "java -cp out/ " (file-name-sans-extension (file-relative-name buffer-file-name projectile-project-root)))))
-
-(defun java-compile-and-run-file ()
-  "This function is meant for compiling and running the current Java file."
-  (interactive)
-  (lsp-format-buffer)
-  (lsp-organize-imports)
-  (save-buffer)
-  (projectile-run-async-shell-command-in-root
-   (concat
-	(concat
-	 (concat "javac src/" (file-relative-name buffer-file-name projectile-project-root))
-	 " -d out/ && ")
-	(concat "java -cp out/ " (file-name-sans-extension (file-relative-name buffer-file-name projectile-project-root))))))
-
-(defun ascii-create ()
-  (interactive)
-  (setq input (thing-at-point 'line t))
-  (kill-whole-line)
-  (setq amount 0)
-  (setq counting t)
-  (setq index 0)
-  (while (< index (length input))
-	(setq c (substring input index (+ index 1)))
-	(if counting
-		(progn
-		  (setq amount (string-to-number c))
-		  (message c)
-		 (when (eq amount 0) (insert "\n"))
-		 )
-	   (insert (make-string amount (string-to-char c)))
-	   )
-	(unless (eq amount 0) (if counting (setq counting nil) (setq counting t)))
-	(setq index (+ index 1))
-	))
+(require 'init-java)
 
 ;; Setup general
 (use-package general
